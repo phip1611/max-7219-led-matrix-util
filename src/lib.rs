473244017c786e242dd0c65e75fe817734123f5c
Philipp Schuster <phip1611@gmail.com>
2021-04-26 09:06:45 +0200

    fixed no_std build (v0.2.3)

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![allow(unused_imports)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #[cfg(not(feature = "std"))]
+#[cfg_attr(not(feature = "std"), macro_use)]
 extern crate alloc;
 #[cfg(not(feature = "std"))]
 use alloc::vec::Vec;
